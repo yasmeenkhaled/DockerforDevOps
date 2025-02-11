33338bf74b4de15b769d3985ca6ab84157d144f3
yasmeenkhaled <yasmeenkhaledgg@gmail.com>
2025-02-11 19:25:27 +0200

    Added connect to MongoDB database

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 //Create Simple server
 //import to 
 const express = require('express');
+const mongoose = require('mongoose'); 
 
 //Initilization
 const app = express();
+
+//Connect to db
+const db_user ='root' ;
+const db_password = 'example';
+const db_port = 27017;    
+const db_host = 'mongo';
+const uri = `mongodb://${db_user}:${db_password}@${db_host}:${db_port}`;
+mongoose.connect(uri).then(() => console.log('Connected to MongoDB')).catch(err => console.log(err));
+
 //Poer that Server will run it  
 const port = process.env.port || 8000;  //by defult 8000 but can use any port 
 
